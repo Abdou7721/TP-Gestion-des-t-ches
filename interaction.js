8c9a1cd6d845cd758db68800e978e545e5abc928
Aboudou <doolcoumba@gmail.com>
2024-04-06 18:27:34 +0000

    deuxième changement

--- interaction.js
+++ interaction.js
@@ -7,23 +7,35 @@ document.getElementById("task").addEventListener("input", function() {
 
     }
 });
+function afficherCalendrier() {
+    const dialogue = document.getElementById("datePicker");
+    dialogue.style.display = "block";
+}
 
 function entrer(){
     const nouvelElement=document.getElementById("task").value;
     
     if(nouvelElement.trim() !== ""){
-       val=true;
+       
         const list=document.getElementById("liste");
         const Element=document.createElement("li");
+        const calendar_img= document.getElementById("agenda");
+        const sup= document.createElement("button");
+        const selectedDate = document.getElementById("date_text");
+        const calendar= document.getElementById("datePicker");
         const image= document.getElementById("im");
         const image1= document.getElementById("im1");
+        
         Element.innerText= nouvelElement;
-        const sup= document.createElement("button");
+        selectedDate.textContent=" mon p ";
+        selectedDate.style.display='block'
+        
+        calendar_img.style.display='block'
         sup.textContent= "X";
         sup.onclick= function(){
             Element.remove();
             image.style.display = 'none';
-            image1.src = image1.src;
+            
             // Afficher l'image avec une animation
             var style = document.createElement('style');
             style.innerHTML = `
@@ -56,6 +68,8 @@ function entrer(){
             
             
         };
+        Element.appendChild(calendar_img);
+        Element.appendChild(selectedDate);
         Element.appendChild(sup);
         list.appendChild(Element);
         image.style.display='block';
@@ -64,3 +78,18 @@ function entrer(){
     }
    
 }
+const selectedDate = document.getElementById("date_text");
+ const calendar= document.getElementById("datePicker");
+ const calendar_img= document.getElementById("agenda");
+calendar.addEventListener('change', function() {
+    // Récupération de la date sélectionnée
+    var reup = calendar.value;
+    
+    calendar_img.style.display='none'
+    calendar.style.display='none'
+    // Affichage de la date sélectionnée dans le paragraphe
+    selectedDate.textContent = "échéance:" + reup;
+});
+document.getElementById("agenda").addEventListener("click", function() {
+    afficherCalendrier();
+});
